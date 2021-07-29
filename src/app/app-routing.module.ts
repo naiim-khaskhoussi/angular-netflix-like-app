a08bbd0e40ea790a157be6ca7ffce9720abe3d58
Naiim <khaskhoussi.naiim@gmail.com>
2021-07-29 19:16:59 +0200

    First commit, adding navbar, sigin and signup components
    Signed-off-by: Naiim <khaskhoussi.naiim@gmail.com>

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SigninFormComponent } from './signin-form/signin-form.component';
+import { SignupFormComponent } from './signup-form/signup-form.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'login', component: SigninFormComponent },
+  { path: 'signup', component: SignupFormComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { CarouselComponent } from './carousel/carousel.component';
+import { SigninFormComponent } from './signin-form/signin-form.component';
+import { FooterComponent } from './footer/footer.component';
+import { SignupFormComponent } from './signup-form/signup-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    CarouselComponent,
+    SigninFormComponent,
+    FooterComponent,
+    SignupFormComponent
   ],
   imports: [
     BrowserModule,
